32049ae460e69e3f176953079ff3ab4aaa9a6658
David Chea <chea.david.pro@gmail.com>
2019-08-10 18:34:20 +0200

    First commit

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Hide the status bar
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
-
-
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    // Identify the button that was tapped
+    enum ButtonType: Int {
+        case camera = 0, album
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Disable the camera button if the device doesn't have a camera
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    // Trigger when the user tapped for picking an image
+    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        // Set the delegate to this controller
+        imagePicker.delegate = self
+        
+        // Set the image's source type
+        switch (ButtonType(rawValue: sender.tag)!) {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .album:
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Retrieve and set the image to the view
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        
+        // Dismiss the image picker's view
+        dismiss(animated: true, completion: nil)
+    }
 }
-
